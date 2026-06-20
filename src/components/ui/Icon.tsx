9c055530e548624a315636e706d8d97da9114539
DevvyyXYZ <devvyydev@protonmail.com>
2026-06-20 08:19:26 +0100

    feat: add home and refresh icons to icon set

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -47,7 +47,9 @@ export type IconName =
   | "bell"
   | "user"
   | "help-circle"
-  | "message-square";
+  | "message-square"
+  | "home"
+  | "refresh";
 
 interface IconProps {
   name: IconName;
@@ -285,6 +287,18 @@ const PATHS: Record<IconName, ReactNode> = {
   "message-square": (
     <>
       <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
+    </>
+  ),
+  home: (
+    <>
+      <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <polyline points="9 22 9 12 15 12 15 22" />
+    </>
+  ),
+  refresh: (
+    <>
+      <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
+      <path d="M21 3v5h-5" />
     </>
   ),
 };
